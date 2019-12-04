95dc9d67bd6916a68e5e1b752e724ba391722d2a
Fabricio Salgado <chocolate.fabricio@gmail.com>
2019-12-04 18:05:41 -0300

    meu primeiro versionamento

---
+++ src/test/java/com/fabricio/mpp/MeuProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabricio.mpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
